e6a31ab2a1f4f821555fa791a6f3cb3633fc21eb
sinhaGuild <anurag.r.sinha@gmail.com>
2022-10-01 22:12:16 -0400

    Mongo seeder

---
+++ data/mongo/a01_createDB.js
@@ -0,0 +1,71 @@
+db.auth("mongo", "mongo");
+baserepo = db.getSiblingDB("BaseRepository");
+baserepo.createCollection("posts");
+baserepo.createCollection("weather24h", {
+  timeseries: {
+    timeField: "timestamp",
+    metaField: "data",
+    granularity: "hours",
+  },
+  expireAfterSeconds: 86400,
+});
+
+//use faker to seed
+try {
+  baserepo.posts.insertMany([
+    {
+      userId: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    },
+    {
+      userId: 1,
+      title: "qui est esse",
+      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+    },
+    {
+      userId: 1,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+    {
+      userId: 1,
+      title: "eum et est occaecati",
+      body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
+    },
+    {
+      userId: 1,
+      title: "nesciunt quas odio",
+      body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque",
+    },
+    {
+      userId: 1,
+      title: "dolorem eum magni eos aperiam quia",
+      body: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae",
+    },
+    {
+      userId: 1,
+      title: "magnam facilis autem",
+      body: "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
+    },
+    {
+      userId: 1,
+      title: "dolorem dolore est ipsam",
+      body: "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae",
+    },
+    {
+      userId: 1,
+      title: "nesciunt iure omnis dolorem tempora et accusantium",
+      body: "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas",
+    },
+    {
+      userId: 1,
+      title: "optio molestias id quia eum",
+      body: "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error",
+    },
+  ]);
+  console.log(JSON.printify(baserepo.posts.find(), null, 2));
+} catch (e) {
+  print(e);
+}
